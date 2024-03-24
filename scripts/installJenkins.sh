7143cdef6dfd5bf753a6fc74cd4ddb537dfd8566
gowdanavya97 <128294280+gowdanavya97@users.noreply.github.com>
2024-03-24 15:16:43 +0530

    Update installJenkins.sh

--- scripts/installJenkins.sh
+++ scripts/installJenkins.sh
@@ -36,7 +36,7 @@ install_centos() {
 #Set JAVA_HOME & MAVEN_HOME as environment variables on Jenkins machine
 #mkdir -p /home/backup
 #cp -p /etc/profile /home/backup/profile_`date +%d%b%Y-%H%M`
-#echo "MAVEN_HOME=/opt/apache-maven-3.8.3" >> /etc/profile
+#echo "MAVEN_HOME=/opt/apache-maven-3.9.6" >> /etc/profile
 #echo "JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> /etc/profile
 #echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/profile
 #echo "PATH=\$JAVA_HOME/bin:\$MAVEN_HOME/bin:\$PATH" >> /etc/profile
